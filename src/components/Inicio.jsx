adf47e9b5845d94e54fe92514d66cb7226ebe47f
ViviMoya <v.moyacapitan@gmail.com>
2021-10-19 13:52:46 -0300

    pagina inicio, y vista mesero

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,20 +3,24 @@ import { Link } from 'react-router-dom'
 
 const Inicio = () => {
    return (
-      <div>
+      <div className="container">
+         <div className="row  mt-6">
+            <div className="col-6">
         <header><h1>BIENVENIDO</h1></header>
          <main><h3>Escoge tu rol:</h3>
-         <div>
+         <div className="d-flex gap-2 justify-content-start">
             <Link to='/informacion'>
-            <button>MESERO</button>
+            <button className="btn btn-lg btn-secondary text-dark">MESERO</button>
             </Link>
-            </div>
-         <div>
-         <Link>
-            <button>COCINERO</button>
+         <Link to=''>
+            <button className="btn btn-lg btn-secondary text-dark">COCINERO</button>
             </Link>
             </div></main>
-         <footer> <p> Que tengas un buen día de trabajo en <span>Burguer-Queen.-</span> </p></footer>
+          
+          
+            </div>
+         </div>
+         <footer className="col-12 mb-0"> <p> Que tengas un buen día de trabajo en <span>Burguer-Queen.-</span> </p></footer>
       </div>
    )
 }
